ac9be62bb55b4626232566fe6bfb52864779aee5
Casper <slei.casper@gmail.com>
2017-11-02 13:45:57 +0800

    fix ident

--- jsvmdebug.py
+++ jsvmdebug.py
@@ -1,4 +1,5 @@
 import gdb,re
+import traceback
 
 class Pasciistr(gdb.Command):
 	def __init__(self):
@@ -6,56 +7,62 @@ class Pasciistr(gdb.Command):
 		self.char_PN = re.compile(r'\'([\w\\]+)\'')
 		self.decimal_PN = re.compile(r'^[0-9]+$')
 	def invoke(self, args, from_tty):
-		argv = gdb.string_to_argv(args)
-		ptr = str( gdb.parse_and_eval(argv[0]) )
+		try:
+			argv = gdb.string_to_argv(args)
+			ptr = str( gdb.parse_and_eval(argv[0]) )
 
-		if len( self.decimal_PN.findall(ptr) ) == 1:
-			ptr = int(ptr)
-		else:
-			ptr = int(ptr, 16)
-		if len( self.decimal_PN.findall(argv[1]) ) == 1:
-			length = int(argv[1])
-		else:
-			length = int(argv[1], 16)
-		if length <= 0:
-			length = 100
-		if length > 100:
-			length = 100
-		result = ''
-		for i in range(length):
-			resstr = gdb.execute("x/1c" + str(ptr), to_string = True)
-			if len(self.char_PN.findall(resstr)) == 1:
-				ch = self.char_PN.findall(resstr)[0]
-				result += ch
+			if len( self.decimal_PN.findall(ptr) ) == 1:
+				ptr = int(ptr)
 			else:
-				print("re find error")
-				return
-			ptr += 1
-		print(result)
+				ptr = int(ptr, 16)
+			if len( self.decimal_PN.findall(argv[1]) ) == 1:
+				length = int(argv[1])
+			else:
+				length = int(argv[1], 16)
+			if length <= 0:
+				length = 100
+			if length > 100:
+				length = 100
+			result = ''
+			for i in range(length):
+				resstr = gdb.execute("x/1c" + str(ptr), to_string = True)
+				if len(self.char_PN.findall(resstr)) == 1:
+					ch = self.char_PN.findall(resstr)[0]
+					result += ch
+				else:
+					print("re find error")
+					return
+				ptr += 1
+			print(result)
+		except:
+                        traceback.print_exc()
 
 
 class Getjsname(gdb.Command):
 	def __init__(self):
 		super(self.__class__, self).__init__("getjsname", gdb.COMMAND_USER)
 	def invoke(self, args, from_tty):
-		argv = gdb.string_to_argv(args)
-		if len(argv) != 1:
-			print("argv error")
-			return
+		try:
+			argv = gdb.string_to_argv(args)
+			if len(argv) != 1:
+				print("argv error")
+				return
 
-		maxidx = int( str(gdb.parse_and_eval("(script.ptr).natoms_")) )
-		idx = abs(int(argv[0]))
-		if idx >= maxidx:
-			print("idx too big")
-			return
-		length = gdb.execute("p (*(script->getAtom(%d).value)).d.u1.length"%idx, to_string=True)
-		length = str(length)
-		if len(re.compile(r'^\$[0-9]+ = ([0-9]+)$').findall(length)) == 1:
-			length = re.compile(r'^\$[0-9]+ = ([0-9]+)$').findall(length)[0]
-			gdb.execute("pasciistr &(*(script->getAtom(%d).value)).d.inlineStorageLatin1 %s"%(idx, length))
-		else:
-			print("re find error")
-			return
+			maxidx = int( str(gdb.parse_and_eval("(script.ptr).natoms_")) )
+			idx = abs(int(argv[0]))
+			if idx >= maxidx:
+				print("idx too big")
+				return
+			length = gdb.execute("p (*(script->getAtom(%d).value)).d.u1.length"%idx, to_string=True)
+			length = str(length)
+			if len(re.compile(r'^\$[0-9]+ = ([0-9]+)$').findall(length)) == 1:
+				length = re.compile(r'^\$[0-9]+ = ([0-9]+)$').findall(length)[0]
+				gdb.execute("pasciistr &(*(script->getAtom(%d).value)).d.inlineStorageLatin1 %s"%(idx, length))
+			else:
+				print("re find error")
+				return
+		except:
+                        traceback.print_exc()
 
 def shiftendian(aNum):
     result = 0
@@ -276,42 +283,45 @@ class Pbytecode(gdb.Command):
 			0xe5:{'name':'JSOP_NOP_DESTRUCTURING', 'codelen':'1', 'isstring':'1'},
 			0xe4:{'name':'JSOP_TOSTRING', 'codelen':'1', 'isstring':'1'}}
 	def invoke(self, args, from_tty):
-		argv = gdb.string_to_argv(args)
-		bytecode = gdb.parse_and_eval("$bytecode")
-		bytecode = int(bytecode)
-		if bytecode in self._BYTECODES:
-			codeinfo = self._BYTECODES[bytecode]
-			bytecodeptr = gdb.parse_and_eval("$bytecodeptr")
-			bytecodeptr = self.hex_PN.findall(str(bytecodeptr))[0]
-			bytecodearg= gdb.parse_and_eval("$bytecodearg")
-			bytecodearg = shiftendian(int(bytecodearg))
-			codelen = codeinfo['codelen']
+		try:
+			argv = gdb.string_to_argv(args)
+			bytecode = gdb.parse_and_eval("$bytecode")
+			bytecode = int(bytecode)
+			if bytecode in self._BYTECODES:
+				codeinfo = self._BYTECODES[bytecode]
+				bytecodeptr = gdb.parse_and_eval("$bytecodeptr")
+				bytecodeptr = self.hex_PN.findall(str(bytecodeptr))[0]
+				bytecodearg= gdb.parse_and_eval("$bytecodearg")
+				bytecodearg = shiftendian(int(bytecodearg))
+				codelen = codeinfo['codelen']
 
-			if codelen == '1':
-				mask = 0
-			elif codelen == '2':
-				mask = 0xff
-			elif codelen == '3':
-				mask = 0xffff
-			elif  codelen == '4':
-				mask = 0xffffff
-			elif  codelen == '5':
-				mask = 0xffffffff
+				if codelen == '1':
+					mask = 0
+				elif codelen == '2':
+					mask = 0xff
+				elif codelen == '3':
+					mask = 0xffff
+				elif  codelen == '4':
+					mask = 0xffffff
+				elif  codelen == '5':
+					mask = 0xffffffff
+				else:
+					print('error')
+					exit(0)
+				bytecodearg = bytecodearg & mask
+				if codeinfo['isstring'] == '1' and codelen != '1':
+					othermsg = str(gdb.execute("getjsname %d"%bytecodearg, to_string = True)).strip()
+				else:
+					othermsg = ''
+				print("%-16s %-25s 0x%-8x %02x %010s%-25s"%(bytecodeptr, codeinfo['name'], bytecodearg, bytecode, "", othermsg))
+				gdb.execute('set $codelength = %s'%(codelen))
 			else:
-				print('error')
+				print("bytecode: %02x"%bytecode)
+				print("wrong bytecode")
 				exit(0)
-			bytecodearg = bytecodearg & mask
-			if codeinfo['isstring'] == '1' and codelen != '1':
-				othermsg = str(gdb.execute("getjsname %d"%bytecodearg, to_string = True)).strip()
-			else:
-				othermsg = ''
-			print("%-16s %-25s 0x%-8x %02x %010s%-25s"%(bytecodeptr, codeinfo['name'], bytecodearg, bytecode, "", othermsg))
-			gdb.execute('set $codelength = %s'%(codelen))
-		else:
-			print("bytecode: %02x"%bytecode)
-			print("wrong bytecode")
-			exit(0)
-		return
+			return
+		except:
+                        traceback.print_exc()
 Pasciistr()
 Getjsname()
 Pbytecode()
